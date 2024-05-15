3d18842a9b216bd917557c580567545962f3a62e
jacking75 <jacking75@gmail.com>
2024-05-15 20:14:21 +0900

    redis 추가 중

--- Redis.md
+++ Redis.md
@@ -4,3 +4,27 @@
 - Boost 라이브러리와 별도로 `OpenSSL` 라이브러리가 필요하다
 - 1.84 기준 [문서](https://www.boost.org/doc/libs/1_84_0/libs/redis/doc/html/index.html )
 
+## 사용 준비
+`Redis 6.0` 이상 버전만 사용할 수 있다.  
+Redis 라이브러리를 사용하려면 `OpenSSL` 라이브러리를 설치해야한다.  
+Redis 라이브러리쪽에서 무조건 OpenSSL을 사용하고 있다.  
+ 
+### Windows
+`vcpkg`로 OpenSSL을 설칳라는 것이 가장 간단하다.  
+vcpkg에 대해서 잘 모른다면 구글링으로 알아보기 바란다. 한글로된 문서도 꽤 많다. 
+  
+동적 라이브러리로 설치하기  
+```
+vcpkg install openssl:x64-windows
+```  
+동적 라이브러리로 설치하기
+```
+vcpkg install boost-windows:x64-windows-static
+```   
+  
+정적 라이브러리 설치로 사용하려고 했지만 링크 에러로 빌드가 되지 않았다.   
+**동적 라이브러리로 설치하기를 추천한다**   
+  
+만약 직접 OpenSSl을 빌드하고 싶다면 이 글을 참고하기 바란다.  
+[(일어) Windows 에서 OpenSSL 빌드하기](https://qiita.com/etaka/items/7a0784325c1f8c01ae41 )
+  
---
+++ redis/main.cpp
@@ -0,0 +1,46 @@
+﻿#include <boost/redis/config.hpp>
+#include <boost/redis/connection.hpp>
+//#include <boost/asio.hpp>
+//#include <boost/asio/detached.hpp>
+
+#include <iostream>
+
+//namespace asio = boost::asio;
+//using boost::redis::connection;
+//using boost::redis::request;
+//using boost::redis::response;
+//using boost::redis::config;
+
+int main()
+{
+    try {
+        boost::redis::config cfg;
+        cfg.use_ssl = false;
+        cfg.addr.host = "127.0.0.1";
+        cfg.addr.port = 6379;
+
+  
+        //boost::redis::request req;
+        //req.push("PING", "Hello world");
+
+
+        //boost::redis::response<std::string> resp;
+        /*
+        asio::io_context ioc;
+        boost::redis::connection conn{ ioc };
+        conn.async_run(cfg, {}, asio::detached);
+
+        conn.async_exec(req, resp, [&](auto ec, auto) {
+            if (!ec)
+                std::cout << "PING: " << std::get<0>(resp).value() << std::endl;
+            conn.cancel();
+            });
+
+        ioc.run();*/
+
+    }
+    catch (std::exception const& e) {
+        std::cerr << "Error: " << e.what() << std::endl;
+        return 1;
+    }
+}
